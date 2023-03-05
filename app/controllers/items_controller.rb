45a498a372edd019b97f5e7edc8d00b141bd4ab5
savannah-yahsuanlin <savannahwang75@gmail.com>
2023-03-05 20:05:15 +0800

    show private def

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,11 @@
 class ItemsController < ApplicationController
+	before_action :find_item, only: [:show, :edit, :update, :destroy]
 
 	def index
-		@items = Item.all.order('created_at ASC')
+		@items = Item.all
+	end
+
+	def show
 	end
 
 	def new
@@ -21,5 +25,9 @@ class ItemsController < ApplicationController
 
 	def item_params
 		params.require(:item).permit(:title, :description)
+	end
+
+	def find_item
+		@item = Item.find(params[:id])
 	end
 end
