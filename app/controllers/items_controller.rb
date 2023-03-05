c5b34daf354723bcf036919a0aff9a1bf82004df
savannah-yahsuanlin <savannahwang75@gmail.com>
2023-03-05 19:45:16 +0800

    new create form

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,7 @@
 class ItemsController < ApplicationController
 
-	def index 
+	def index
+		@items = Item.all.order('created_at ASC')
 	end
 
 	def new
@@ -8,5 +9,17 @@ class ItemsController < ApplicationController
 	end
 
 	def create
+		@item = Item.new(item_params)
+		if @item.save
+			redirect_to root_path
+		else
+			render 'new'
+		end
+	end
+
+	private
+
+	def item_params
+		params.require(:item).permit(:title, :description)
 	end
 end
